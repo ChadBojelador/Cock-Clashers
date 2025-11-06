2de0e0b54bb6422d5f19ca375f610c04cc81b4fb
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-11-06 08:51:48 +0800

    feat: established rooster class

---
+++ src/Rooster.java
@@ -0,0 +1,37 @@
+public class Rooster {
+    private String name;
+    private int hp;
+    private int attack;
+    private int defense;
+
+    public Rooster(String name, int hp, int attack, int defense){
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getHp(){
+        return hp;
+    }
+
+
+    public void takeDamage(int damage) {
+        hp -= damage;
+        if (hp < 0) hp = 0;
+    }
+
+    public boolean isFainted() {
+        return hp <= 0;
+    }
+
+    public void attack(Rooster enemy) {
+        int damage = Math.max(1, attack - enemy.defense);
+        enemy.takeDamage(damage);
+        System.out.println(name + " attacks " + enemy.getName() + " for " + damage + " damage!");
+    }
+}
+
